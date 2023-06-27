6ef9110fc95faeb77dcd9fa19acc57f5282969f1
Joao Lino <linusmannuel@gmail.com>
2023-06-27 18:33:02 +0100

    forEach mostrando videos na tela

--- app/mostrarVideos.js
+++ app/mostrarVideos.js
@@ -1,18 +1,17 @@
 import { conectaAPI } from './conectaAPI.js';
 const lista = document.querySelector('[data-lista]');
 
-function constroiCard() {
+function constroiCard(titulo, descricao, url, imagem) {
 	const video = document.createElement('li');
-	video.className('videos__item');
-	video.innerHTML = `
-  <iframe width="100%" height="72%" src="https://www.youtube.com/embed/OrnUhR41MYI"
-    title="Voltando ao mercado após a maternidade: Ana Silvério" frameborder="0"
+	video.classList.add('videos__item');
+	video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
     <div class="descricao-video">
-    <img src="./img/logo.png" alt="logo canal alura">
-    <h3>Voltando ao mercado após a mmaternidade: Ana Silvério</h3>
-    <p>618 visualizações</p>
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
     </div>
   `;
 
@@ -20,5 +19,17 @@ function constroiCard() {
 }
 
 async function listaVideo() {
-	const lista = await conectaAPI.listaVideos();
+	const listaApi = await conectaAPI.listaVideos();
+	listaApi.forEach((elemento) =>
+		lista.appendChild(
+			constroiCard(
+				elemento.titulo,
+				elemento.descricao,
+				elemento.url,
+				elemento.imagem
+			)
+		)
+	);
 }
+
+listaVideo();
