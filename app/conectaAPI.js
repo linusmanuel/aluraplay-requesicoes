9add0cd25042a95f4fd8f3f16208f9c2240ad618
Joao Lino <linusmannuel@gmail.com>
2023-06-29 13:43:03 +0100

    requisicoes post

--- app/conectaAPI.js
+++ app/conectaAPI.js
@@ -5,6 +5,26 @@ async function listaVideos() {
 	return conexaoConvertida;
 }
 
+async function criarVideo(titulo, url, descricao, imagem) {
+	const conexao = await fetch('http://localhost:3000/videos', {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify({
+			titulo: titulo,
+			descricao: `${descricao} mil visualizações`,
+			url: url,
+			imagem: imagem,
+		}),
+	});
+
+	const conexaoConvertida = await conexao.json();
+
+	return conexaoConvertida;
+}
+
 export const conectaAPI = {
 	listaVideos,
+	criarVideo,
 };
